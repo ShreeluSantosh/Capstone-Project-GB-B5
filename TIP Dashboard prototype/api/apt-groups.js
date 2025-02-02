e7e6ff79ffa42c793cbb4585283c947596ba2198
Lun4rC1ph3r <94289402+ShreeluSantosh@users.noreply.github.com>
2025-02-02 16:43:22 +0530

    Add /api folder

---
+++ TIP Dashboard prototype/api/apt-groups.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+
+const app = express();
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://tweetbeacon-demo.vercel.app/'],
+    optionsSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+const mongoURI = 'mongodb+srv://shreelu:2A8bc9ws@cluster0.irbc4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0>';
+const client = new MongoClient(mongoURI);
+
+let db;
+const connectToDatabase = async () => {
+  try {
+    await client.connect();
+    db = client.db('Threat_Actors'); // Replace with your database name
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+};
+
+connectToDatabase();
+
+// API route to fetch all APT groups
+app.get('/api/apt-groups', async (req, res) => {
+  try {
+    const aptGroups = await db.collection('APTGroups').find({}).toArray(); // Replace with your collection name
+    res.json(aptGroups);
+  } catch (err) {
+    console.error('Error fetching APT groups:', err);
+    res.status(500).send('Error fetching APT groups');
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Gracefully handle server shutdown
+process.on('SIGINT', async () => {
+  await client.close();
+  console.log('MongoDB connection closed');
+  process.exit(0);
+});
---
+++ TIP Dashboard prototype/api/tweets.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = 5001;
+
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://tweetbeacon-demo.vercel.app/'],
+    optionsSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
+  
+  app.use(express.json());
+
+const mongoURI = 'mongodb+srv://shreelu:2A8bc9ws@cluster0.irbc4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0>';
+const client = new MongoClient(mongoURI);
+
+app.get('/api/tweets', async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db('Tweets'); 
+    const tweetsCollection = db.collection('Tweets'); 
+    const tweets = await tweetsCollection.find({}).toArray();
+    res.json(tweets);
+  } catch (err) {
+    console.error('Error fetching tweets:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
