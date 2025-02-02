d88c844099b95c67455175aa61b771bf502fec97
Lun4rC1ph3r <94289402+ShreeluSantosh@users.noreply.github.com>
2025-02-02 16:40:41 +0530

    Update sheetService.js

--- TIP Dashboard prototype/src/services/sheetService.js
+++ TIP Dashboard prototype/src/services/sheetService.js
@@ -1,6 +1,6 @@
 export const fetchAPTData = async () => {
   try {
-    const response = await fetch('http://localhost:5000/api/apt-groups'); // Replace with your backend URL
+    const response = await fetch('http://localhost:5000/api/apt-groups' || '/api/apt-groups'); // Replace with your backend URL
     const data = await response.json();
     return data;
   } catch (error) {
