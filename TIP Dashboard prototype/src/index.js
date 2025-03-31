132a05a9b73aadce38357fb1c60adc7472c2c017
Lun4rC1ph3r <94289402+ShreeluSantosh@users.noreply.github.com>
2025-03-31 20:23:51 +0530

    Update index.js

--- TIP Dashboard prototype/src/index.js
+++ TIP Dashboard prototype/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_GorfJMVhW",
+  client_id: "1u03g2n0diall88giqu2dmpt73",
+  redirect_uri: "https://tweetbeacon-demo.vercel.app/",
+  response_type: "code",
+  scope: "email openid phone",
+  loadUserInfo: true,
+  metadata: {
+    issuer: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_GorfJMVhW",
+    authorization_endpoint: "https://us-east-1gorfjmvhw.auth.us-east-1.amazoncognito.com/login",
+    token_endpoint: "https://us-east-1gorfjmvhw.auth.us-east-1.amazoncognito.com/oauth2/token",
+    end_session_endpoint: "https://us-east-1gorfjmvhw.auth.us-east-1.amazoncognito.com/logout",
+    userinfo_endpoint: "https://us-east-1gorfjmvhw.auth.us-east-1.amazoncognito.com/oauth2/userInfo"
+  }
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
